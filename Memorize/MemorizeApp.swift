37df96c020568c55649b8777ffe25773e38663bf
betomoedano <betomoedano@outlook.com>
2021-10-26 04:04:47 -0700

    first commit 🚀

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alberto Moedano on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
